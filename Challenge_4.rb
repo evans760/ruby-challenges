68f60c6f6736dbbc200354632b8f78589525220c
Thomas <evans_760@yahoo.com>
2016-07-06 09:59:00 -0700

    ruby challenges with some of challenge 4

--- Challenge_4.rb
+++ Challenge_4.rb
@@ -1,2 +1,12 @@
-Guessing Game
+number = (1..100).to_a.shuffle.pop
+tries = 0
 
+puts 'guess a number between 1 and 100'
+
+player_guess = gets.chomp
+tries += 1
+
+def guess(number, player_guess, tries)
+  if player_guess.to_i == number.to_i
+    puts'you got it #{number}you got it in #{tries} tries'
+  elsif player_guess.to_i > 100 || player_guess.to_i < 1
